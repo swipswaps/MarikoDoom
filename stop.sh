6232445c32e0a3c033af70def24fbf6bf74459d6
z80z80z80 <z80z80z80@protonmail.com>
2020-02-20 23:21:45 +0100

    implemented a less aggressive way of stopping the server

--- stop.sh
+++ stop.sh
@@ -2,10 +2,9 @@
 # Obviously this is not the best way to stop the server/game but it works for now. 
 # It will also kill all other unprivileged scripts though.
 
-echo WARNING: This script also stops all unprivileged python scripts you are running.
-read -n1 -p 'Do you want to continue? [y/N]: ' doit
+read -n1 -p 'Do you really want to stop the server? [y/N]: ' doit
 case $doit in
-  y|Y) echo && echo Stopping server... && killall python && killall python3 ;;
+  y|Y) echo && echo Stopping server... && pkill "vizdoom" && pkill "python server.py" && pkill "python3 server.py" ;;
   n|N) echo && echo Abort. ;;
   *) echo && echo Abort. ;;
 esac
