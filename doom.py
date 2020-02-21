90b1b8acd70afac676ad2971a149d712ef6b2b03
z80z80z80 <z80z80z80@protonmail.com>
2020-02-21 20:44:55 +0100

    fixed some typos

--- doom.py
+++ doom.py
@@ -242,6 +242,6 @@ if __name__ == "__main__":
         main(args)
 
     except:
-        os.system("rm -rf _vizdom vizdoom.ini vizdoom-crash.log") # don't at me, I know it's dirty...
+        os.system("rm -rf _vizdoom _vizdoom.ini vizdoom-crash.log") # don't @ me, I know it's dirty...
         print("\n *\033[1;31m Server stopped.\033[0;36m")
         
